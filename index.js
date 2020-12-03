817287e1fea5e2330fde7ef3c9cb87adb25bbacf
James Totah <jtotah@usfca.edu>
2020-12-03 09:06:16 -0800

    UPDATE: Added questions and an array of licenses

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 // array of questions for user
 const questions = [
-
+    "Title of the project: ",
+    "Description of project: ",
+    "How is this project installed: ",
+    "How is this project used: ",
+    "Choose a license: ",
+    "List all contributors: ",
+    "Specify testing protocol/instructions for the project: "
 ];
+
+const licenses = [
+    "Apache 2.0 License",
+    "Boost Software License 1.0",
+    "BSD 3-Clause License",
+    "BSD 2-Clause License",
+    "CC0",
+    "Attribution 4.0 International",
+    "Attribution-ShareAlike 4.0 International",
+    "Attribution-NonCommercial 4.0 International",
+    "Attribution-NoDerivates 4.0 International",
+    "Attribution-NonCommmercial-ShareAlike 4.0 International",
+    "Attribution-NonCommercial-NoDerivatives 4.0 International",
+    "Eclipse Public License 1.0",
+    "GNU GPL v3",
+    "GNU GPL v2",
+    "GNU AGPL v3",
+    "GNU LGPL v3",
+    "GNU FDL v1.3",
+    "IBM Public License Version 1.0",
+    "ISC License (ISC)",
+    "The MIT License",
+    "Mozilla Public License 2.0",
+    "Attribution License (BY)",
+    "Open Database License (ODbL)",
+    "Public Domain Dedication and License (PDDL)",
+    "The Perl License",
+    "The Artistic License 2.0",
+    "SIL Open Font License 1.1",
+    "The Unlicense",
+    "The zlib/libpng License"
+]
 
 // function to write README file
 function writeToFile(fileName, data) {
